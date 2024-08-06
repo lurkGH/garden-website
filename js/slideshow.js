f30f987a8a55b9659b9e09801825a10bed17d89a
lurkGH <lurkoasistwitch@gmail.com>
2024-08-06 18:47:07 -0500

    fix: slideshow display issue - Fix to issue where slideshow begins before images are properly loaded.

--- js/slideshow.js
+++ js/slideshow.js
@@ -10,12 +10,8 @@ function slideshow() {
     let elements = document.getElementsByClassName("slideshow");
     if (elements.length > 0) {
         let slideshowElement = elements[0];
-        console.log('Current Index:', currentIndex); // Debugging
-        console.log('Current Image:', images[currentIndex]); // Debugging
         slideshowElement.style.backgroundImage = images[currentIndex];
         currentIndex = (currentIndex + 1) % images.length;
-    } else {
-        console.error('No elements found with class "slideshow"');
     }
     setTimeout(slideshow, 2500); // 1000 = 1 sec
 }
