1bec1b05a3b09975eb6eee813525d12d1f457820
lurkGH <lurkoasistwitch@gmail.com>
2024-08-06 18:28:51 -0500

    feat: add slideshow script

---
+++ js/slideshow.js
@@ -0,0 +1,19 @@
+let images = [
+    "url(../images/slideshow-1.png)",
+    "url(../images/slideshow-2.png)",
+    "url(../images/slideshow-3.png)"
+];
+
+let currentIndex = 0;
+
+function slideshow() {
+    let elements = document.getElementsByClassName("slideshow");
+    if (elements.length > 0) {
+        let slideshowElement = elements[0];
+        slideshowElement.style.backgroundImage = images[currentIndex];
+        currentIndex = (currentIndex + 1) % images.length;
+    }
+    setTimeout(slideshow, 2500); // 1000 = 1 sec
+}
+
+slideshow();
