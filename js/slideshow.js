f5558bcf53f9786f993fc39560eb1ae9f4d42adb
lurkGH <lurkoasistwitch@gmail.com>
2024-08-06 18:43:54 -0500

    fix: slideshow display issue - Fixes relative paths for slideshow images

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,7 +1,7 @@
 let images = [
-    "url(../images/slideshow-1.png)",
-    "url(../images/slideshow-2.png)",
-    "url(../images/slideshow-3.png)"
+    "url(./images/slideshow-1.png)",
+    "url(./images/slideshow-2.png)",
+    "url(./images/slideshow-3.png)"
 ];
 
 let currentIndex = 0;
@@ -10,8 +10,12 @@ function slideshow() {
     let elements = document.getElementsByClassName("slideshow");
     if (elements.length > 0) {
         let slideshowElement = elements[0];
+        console.log('Current Index:', currentIndex); // Debugging
+        console.log('Current Image:', images[currentIndex]); // Debugging
         slideshowElement.style.backgroundImage = images[currentIndex];
         currentIndex = (currentIndex + 1) % images.length;
+    } else {
+        console.error('No elements found with class "slideshow"');
     }
     setTimeout(slideshow, 2500); // 1000 = 1 sec
 }
