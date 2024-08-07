b112e6e0e939adc3da3186a2ddb697d9cceb0c33
lurkGH <lurkoasistwitch@gmail.com>
2024-08-07 18:23:44 -0500

    refactor: make preload function for slideshow images

--- js/slideshow.js
+++ js/slideshow.js
@@ -4,6 +4,33 @@ const slides = [
     "url(./images/slideshow-3.png)"
 ];
 
+function preload(_slides, callback) {
+    let loadedImagesCount = 0;
+    let imageObjects = [];
+    for (let i = 0; i < _slides.length; i++) {
+        let imageLink = _slides[i];
+        let img = new Image();
+        // Extracts the pathname, excluding url()
+        img.src = imageLink.slice(5, -1);
+        // Adds to count when image has loaded
+        img.onload = function() {
+            loadedImagesCount++;
+            if (loadedImagesCount === _slides.length) {
+                callback();
+            }
+        };
+        // Error check to callback if image fails to load
+        img.onerror = function() {
+            console.error("Failed to load image");
+            loadedImagesCount++;
+            if (loadedImagesCount === _slides.length) {
+                callback();
+            }
+        };
+        imageObjects.push(img);
+    }
+}
+
 function startSlideshow() {
     let slideshow = document.getElementsByClassName("slideshow")[0];
     let currIndex = 0;
@@ -15,11 +42,11 @@ function startSlideshow() {
             currIndex++;
         }
         slideshow.style.backgroundImage = slides[currIndex];
-        setTimeout(showNextSlide, 2500);
+        setTimeout(showNextSlide, 2500); // 1000 = 1 second
     }
     showNextSlide();
 }
 
 window.onload = function() {
-    startSlideshow();
+    preload(slides, startSlideshow);
 }
