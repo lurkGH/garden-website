f512211c8de490a8091b0979628f8679c09bc359
lurkGH <lurkoasistwitch@gmail.com>
2024-08-06 20:03:57 -0500

    fix: slideshow display issue - Issue where slideshow images did not load in fully before slideshow displayed them, leading to missing images on first cycle.

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,19 +1,40 @@
 let images = [
-    "url(./images/slideshow-1.png)",
-    "url(./images/slideshow-2.png)",
-    "url(./images/slideshow-3.png)"
+    "./images/slideshow-1.png",
+    "./images/slideshow-2.png",
+    "./images/slideshow-3.png"
 ];
 
+let preloadedImages = [];
 let currentIndex = 0;
 
-function slideshow() {
+function preloadImages(imageArray, callback) {
+    let loadedImagesCount = 0;
+    let totalImages = imageArray.length;
+
+    for (let i = 0; i < totalImages; i++) {
+        preloadedImages[i] = new Image();
+        preloadedImages[i].src = imageArray[i];
+        preloadedImages[i].onload = function() {
+            loadedImagesCount++;
+            if (loadedImagesCount === totalImages) {
+                callback();
+            }
+        };
+    }
+}
+
+function startSlideshow() {
     let elements = document.getElementsByClassName("slideshow");
     if (elements.length > 0) {
         let slideshowElement = elements[0];
-        slideshowElement.style.backgroundImage = images[currentIndex];
-        currentIndex = (currentIndex + 1) % images.length;
+        function showNextImage() {
+            slideshowElement.style.backgroundImage = `url(${images[currentIndex]})`;
+            currentIndex = (currentIndex + 1) % images.length;
+            setTimeout(showNextImage, 2500); // 1000 = 1 sec
+        }
+        showNextImage();
     }
-    setTimeout(slideshow, 2500); // 1000 = 1 sec
 }
 
-slideshow();
+// Preloads images and starts slideshow once done
+preloadImages(images, startSlideshow);
