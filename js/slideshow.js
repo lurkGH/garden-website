4a8f2e5e40b22d524543c64e0591431bbf0fb9d7
lurkGH <lurkoasistwitch@gmail.com>
2024-08-07 17:56:34 -0500

    - refactor of slideshow.js - add transition for slideshow - modify slideshow images for better mobile appearance

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,40 +1,25 @@
-let images = [
-    "./images/slideshow-1.png",
-    "./images/slideshow-2.png",
-    "./images/slideshow-3.png"
+const slides = [
+    "url(./images/slideshow-1.png)",
+    "url(./images/slideshow-2.png)",
+    "url(./images/slideshow-3.png)"
 ];
 
-let preloadedImages = [];
-let currentIndex = 0;
-
-function preloadImages(imageArray, callback) {
-    let loadedImagesCount = 0;
-    let totalImages = imageArray.length;
-
-    for (let i = 0; i < totalImages; i++) {
-        preloadedImages[i] = new Image();
-        preloadedImages[i].src = imageArray[i];
-        preloadedImages[i].onload = function() {
-            loadedImagesCount++;
-            if (loadedImagesCount === totalImages) {
-                callback();
-            }
-        };
-    }
-}
-
 function startSlideshow() {
-    let elements = document.getElementsByClassName("slideshow");
-    if (elements.length > 0) {
-        let slideshowElement = elements[0];
-        function showNextImage() {
-            slideshowElement.style.backgroundImage = `url(${images[currentIndex]})`;
-            currentIndex = (currentIndex + 1) % images.length;
-            setTimeout(showNextImage, 2500); // 1000 = 1 sec
+    let slideshow = document.getElementsByClassName("slideshow")[0];
+    let currIndex = 0;
+    function showNextSlide() {
+        if (currIndex == slides.length - 1) {
+            currIndex = 0;
         }
-        showNextImage();
+        else {
+            currIndex++;
+        }
+        slideshow.style.backgroundImage = slides[currIndex];
+        setTimeout(showNextSlide, 2500);
     }
+    showNextSlide();
 }
 
-// Preloads images and starts slideshow once done
-preloadImages(images, startSlideshow);
+window.onload = function() {
+    startSlideshow();
+}
